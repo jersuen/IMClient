62493cbd96a5842c4a5317a8f7f9450be07e9ed9
JerSuen <jersuen@gmail.com>
2014-05-13 23:50:57 +0800

    创建程序主页面菜单

--- IMClient/src/main/java/com/jersuen/im/MainActivity.java
+++ IMClient/src/main/java/com/jersuen/im/MainActivity.java
@@ -1,33 +1,47 @@
 package com.jersuen.im;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import com.jersuen.im.ui.adapter.FragmentAdapter;
+import com.jersuen.im.ui.view.PageIndicator;
 
 
-public class MainActivity extends Activity {
+/**
+ * 主界面
+ * @author JerSuen
+ */
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
-    @Override
+    private ViewPager viewPager;
+    private PageIndicator indicator;
+    private FragmentPagerAdapter adapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.activity_main_pager);
+        adapter = new FragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        indicator = (PageIndicator) findViewById(R.id.activity_main_pager_indicator);
+        indicator.setViewPager(viewPager);
+
+        findViewById(R.id.activity_main_btn_contact).setOnClickListener(this);
+        findViewById(R.id.activity_main_btn_session).setOnClickListener(this);
+
     }
 
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
@@ -35,4 +49,14 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.activity_main_btn_session:
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.activity_main_btn_contact:
+                viewPager.setCurrentItem(1);
+            break;
+        }
+    }
 }
